1d1878ad32766147e2592387174b4349f902a842
manrajsinghahluwalia <94595941+manrajsinghahluwalia@users.noreply.github.com>
2021-11-22 17:23:41 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -10,6 +10,7 @@ import streamlit as st
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import numpy as np
+import pandas as pd
 
 st.set_page_config(
     page_title="Mortgage Loan Simulator")
